11aa1675a8ca341c2a4de68e7380abab4adf3663
Harrison Termotto <harrison.termotto@stonybrook.edu>
2018-04-06 21:20:20 -0400

    Template for pivot table test cases.

---
+++ xlwings/tests/test_pivot_table.py
@@ -0,0 +1,59 @@
+import xlwings as xw
+
+class test_pivot_table(unittest.TestCase):
+
+				def setUp(self):
+						#Create pivot table from sample sheet with certain data.
+						pass
+
+				def test_pivot_tables(self):
+						pass
+
+				def test_new_pivot_table(self):
+						pass
+
+				def test_name(self):
+						pass
+
+				def test_correct_row_fields(self):
+						pass
+
+				def test_correct_column_fields(self):
+						pass
+
+				def test_correct_page_fields(self):
+						pass
+
+				def test_correct_data_fields(self):
+						pass
+
+				def test_hide_row_field(self):
+						pass
+
+				def test_add_row_field(self):
+						pass
+
+				def test_hide_column_field(self):
+						pass
+
+				def test_add_column_field(self):
+						pass
+
+				def test_add_page_field(self):
+						pass
+
+				def test_hide_page_field(self):
+						pass
+
+				def test_hide_data_field(self):
+						pass
+
+				def test_add_data_field(self):
+						pass
+
+				def test_delete(self):
+						pass
+
+
+if __name__ == '__main__':
+				unittest.main()
